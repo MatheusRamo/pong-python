8cb8983694355eacdc7f1915b5a3db92e3adf622
MatheusRamo <geof.matheus@gmail.com>
2020-10-25 10:35:34 -0300

    add move the ball and border checking

--- pong.py
+++ pong.py
@@ -14,7 +14,7 @@ window.tracer(0)
 player_a = turtle.Turtle()
 player_a.speed(0)
 player_a.shape("square")
-player_a.shapesize(stretch_wid=7, stretch_len=1)
+player_a.shapesize(stretch_wid=5, stretch_len=1)
 player_a.color("yellow")
 player_a.penup()
 player_a.goto(-350, 0)
@@ -23,7 +23,7 @@ player_a.goto(-350, 0)
 player_b = turtle.Turtle()
 player_b.speed(0)
 player_b.shape("square")
-player_b.shapesize(stretch_wid=7, stretch_len=1)
+player_b.shapesize(stretch_wid=5, stretch_len=1)
 player_b.color("yellow")
 player_b.penup()
 player_b.goto(350, 0)
@@ -31,13 +31,18 @@ player_b.goto(350, 0)
 
 # Ball
 ball = turtle.Turtle()
-ball.speed(2)
+ball.speed(0)
 ball.shape("circle")
 ball.color("yellow")
 ball.penup()
 ball.goto(0,0)
 
-# Function
+ball.dx = 1
+ball.dy = 1
+
+
+
+# Functions
 def player_a_up():
     y = player_a.ycor()
     y = y + 15
@@ -70,3 +75,26 @@ window.onkeypress(player_b_down, "Down")
 #Main game loop
 while True:
     window.update()
+
+    # Move the ball
+    ball.setx(ball.xcor() + ball.dx)
+    ball.sety(ball.ycor() + ball.dy)
+
+    # Border checking
+    if ball.ycor() > 290:
+        ball.sety(290)
+        ball.dy = ball.dy * (-1)
+
+    if ball.ycor() < -290:
+        ball.sety(-290)
+        ball.dy = ball.dy * (-1)
+
+    if ball.xcor() > 390:
+        ball.goto(0,0)
+        ball.dx = ball.dx * (-1)
+
+    if ball.xcor() < -390:
+        ball.goto(0,0)
+        ball.dx = ball.dx * (-1)
+
+    # Colision betewen Ball and Player 
